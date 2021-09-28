617d46f4dd856be510473c3147e07994ee1a55df
Vladimir Pyrlik <vladimir.pyrlik@gmail.com>
2021-09-28 20:14:56 +0200

    +

--- hospitaldata.R
+++ hospitaldata.R
@@ -1,7 +1,9 @@
+#  Open with Encoding Windows 1251   # 
+
 ######################################
 #                                    #
-#                                    #
-#                                    #
+#  О риске инфекционного заражения   #
+#      в американских больницах      #
 #                                    #
 #         Владимир Пырлик            #
 #           ЭКОНОМЕТРИКА             #
@@ -34,44 +36,59 @@ library(stargazer)
 ##################################
 # загружаем данные
 setwd("~/ECOX21")
-dta <- read.table("hospitaldata.txt", header=TRUE)
+dta <- read.table("hospitaldata.txt",
+                  header=TRUE)
 
 ##################################
 #           РЕГРЕССИЯ            #
 ##################################
 
-# матрицы с данными
+## работа "вручную"
+# количество наблюдений
 n <- nrow(dta)
+
+# матрицы с данными
 Y <- dta$InfctRsk
 X <- cbind(intercept = rep(1,n),
            stay      = dta$Stay,
            age       = dta$Age,
            beds      = dta$Beds)
 
-hatQxx  <- t(X)%*%X/n
-hatQxy  <- t(X)%*%Y/n
+# компоненты для вычисления оценок
+hatQxx  <- t(X) %*% X / n 
+hatQxy  <- t(X) %*% Y / n
 hatQxx1 <- solve(hatQxx)
 
+# оценки МНК
 hatbeta <- hatQxx1 %*% hatQxy
 
+# вектор остатков
 hatU    <- as.vector(Y - X%*%hatbeta) 
 
-hatV1   <- as.numeric(var(hatU))*hatQxx1
+# не-робастная оценка
+# матрицы ковариаций оценок 
+hatV1   <- var(hatU)*hatQxx1 
 
-hatOmega <- diag(hatU^2)
-hatQxuxu <- t(X)%*%hatOmega%*%X/n
-hatV2    <- hatQxx1 %*% hatQxuxu %*% hatQxx1
+# робастная (метод Уайта) оценка
+# матрицы ковариаций оценок
+hatVxu <- t(X) %*% diag(hatU^2) %*% X/n
+hatV2  <- hatQxx1 %*% hatVxu %*% hatQxx1
 
-SE1 <- sqrt(diag(hatV1)/n)
-SE2 <- sqrt(diag(hatV2)/n)
+# две версии стандартных ошибок
+SE1 <- sqrt(diag(hatV1)/n) # не-робастные SE
+SE2 <- sqrt(diag(hatV2)/n) # робастные SE (White's)
 
+# "таблица" с результатами
 cbind(hatbeta, SE1, SE2)
 
+## работа встроенными средствами
 # оценка МНК, функция lm()
-m1 <- lm(InfctRsk~1+Stay+Age+Beds, data=dta)
+m1 <- lm(InfctRsk ~ 1 + Stay + Age + Beds,
+         data=dta)
 
-# оценка стандартных ошибок, робастная формула
-cf1 <- coeftest(m1, df=Inf, vcov=vcovHC)
+# оценка стандартных ошибок,
+# робастная формула
+cf1 <- coeftest(m1, df=Inf, vcov=vcovHC, type="HC0")
 
 # вывод результатов в таблицу
 stargazer(m1,cf1,type='text')
@@ -79,28 +96,6 @@ stargazer(m1,cf1,type='text')
 ##################################
 #      ПРОВЕРКА ГИПОТЕЗЫ         #
 ##################################
-t1  <- cf1[2,1]/cf1[2,2]
+t1  <- cf1[4,1]/cf1[4,2]
 Pv1 <- 1-pnorm(t1) #1-сторонняя!!!
         
-##################################
-#     АЛЬТЕРНАТИВНАЯ МОДЕЛЬ      #
-##################################
-# В этом пункте вкратце все предыдущие упражнения
-# проделаны для альтернативной модели в log-log форме
-
-plot(x = log(x),y = log(y),
-     main = "Диаграмма рассеивания",
-     xlab = "Доза витамана C, мм, log-шкала",
-     ylab = "Длина зубов, мг, log-шкала")
-b11 <- cov(log(x),log(y))/var(log(x))
-b01 <- mean(log(y)) - b11*mean(log(x))
-abline(b01,b11)
-
-m2  <- lm(log(len)~1+log(dose), data=ToothGrowth)
-cf2 <- coeftest(m2, df=Inf, vcov=vcovHC)
-
-stargazer(m2, cf2,type='text')
-
-t2  <- cf2[2,1]/cf2[2,2]
-Pv2 <- 1-pnorm(t2)
-
